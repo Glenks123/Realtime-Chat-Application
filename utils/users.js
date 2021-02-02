eb793c132be7287802aac3e269c3bed128d68388
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2021-02-02 21:31:55 +0300

    Update users.js

--- utils/users.js
+++ utils/users.js
@@ -6,22 +6,18 @@ let userJoin = (id, username, room) => {
     return user;
 }
 
-// Get current user
+
 let getCurrentUser = id => {
-    //Finds the user from the 'users array' where the user id in that object is equal to the id passed in
     return users.find(user => user.id === id);
 }
 
-// User leaves chat
+
 let userLeave = id => {
-    //Find the index of the user in the users array where the user id is equal to the id passed in 
     const index = users.findIndex(user => user.id === id);
     if (index !== -1){
         return users.splice(index, 1)[0];
     }
 }
-
-// Get users in room
 
 let getRoomUsers = room => {
     return users.filter(user => user.room === room);
