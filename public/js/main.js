6a675dfb2195a3082654927596f658ab17f2e703
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2021-02-02 21:32:43 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,6 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
-// Getting username and room from the URL. EX: http://localhost:3000/chat.html?username=Glen&room=JavaScript
-// Gets Glen and Javascript from the URL
-
 const { username, room } = Qs.parse(location.search, {
     // ignores the ?, & etc.
     ignoreQueryPrefix: true
@@ -26,7 +23,7 @@ socket.on('roomUsers', ({room, users}) => {
 
 // socket.on is used for listening for any incoming messages
 socket.on('message', message => {
-    // message is caught from the server.js and displayed on the web console
+
     //console.log(message);
     outputMessage(message);
 
@@ -36,11 +33,9 @@ socket.on('message', message => {
 
 // Message submit
 chatForm.addEventListener('submit', event => {
-    // this prevents the form from storing the value in an other file which it does by default.
+    
     event.preventDefault();
-    // the message
     const msg = event.target.elements.msg.value;
-    // emiting so that it can be caught by the server and consoled there instead on the web console
     socket.emit('chatMessage', msg);
 
     //clears text area
